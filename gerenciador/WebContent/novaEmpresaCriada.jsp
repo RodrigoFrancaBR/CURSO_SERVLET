c65a5c0a92663322b7eb4218e014593285ddbaf6
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-01-23 22:14:22 -0200

    aula 5 finalizada

---
+++ gerenciador/WebContent/novaEmpresaCriada.jsp
@@ -0,0 +1,19 @@
+
+<%
+    String nomeEmpresa = (String) request.getAttribute("empresa");
+    System.out.println(nomeEmpresa);
+%>
+
+<html>
+<body>
+
+	Empresa " +
+	<% out.println(nomeEmpresa); %>
+	+ " cadastrada com sucesso!
+
+	</br> Empresa " +
+	<%=(nomeEmpresa) %>
+	+ " cadastrada com sucesso!
+
+</body>
+</html>
--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,9 +28,13 @@ public class NovaEmpresaServlet extends HttpServlet {
 		banco.adiciona(empresa);
 
 		System.out.println("Cadastrando nova empresa");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		// pendurar um objeto na requisição
+		request.setAttribute("empresa", empresa.getNome());
+		/// para frente a requisição
+        rd.forward(request, response);
 
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
 	}
 
 }
